bd1e10ab918a30061f8668701ea76fc4c7fedaf9
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2023-02-12 00:38:24 +0600

    1.0

---
+++ Basic PHP/var and cons.php
@@ -0,0 +1,15 @@
+<?php
+//variable declare
+$read='mina';
+echo $read;
+echo nl2br("\n\n");
+$write='trina';
+echo $write;
+
+//contant declare
+
+define('PI',3.14159);
+echo nl2br("\n\n");
+echo "value of PI= ".PI;
+
+?>
