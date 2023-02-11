1504aded9e4713fdba38855c181651f64ca4b9fc
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2023-02-12 00:37:52 +0600

    1.0

---
+++ Basic PHP/my first code.php
@@ -0,0 +1,15 @@
+<?php
+$age=16;
+$cun='Bangladesh';
+$name='Robin';
+
+echo nl2br("One line.\nAnother line.");
+echo "Hello World! <br>";
+echo nl2br("Halim.\nanother.");
+echo "Hello {$name}</br>";
+echo "Roll no is {$age}</br>";
+
+echo "My name is {$name}, age is {$age}</br>";
+echo "<br>";
+echo "My name is {$name}, age is {$age}";
+?>
