776e855433a66de6e697e22ef5ab91b6a13e3da6
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2023-02-13 00:52:25 +0600

    1.0

---
+++ Basic PHP/while loop tricks.php
@@ -0,0 +1,20 @@
+<?php
+$i=1;
+
+while($i<=10){
+    echo "$i <br>";
+    $i++;
+}
+//another
+echo "New one more system while loop <br>";
+$j=0;
+while($j++<10){
+    echo "$j <br>";
+}
+
+echo "New one more system while loop <br>";
+$z=0;
+while(++$z <10){
+    echo "$z <br>";
+}
+?>
