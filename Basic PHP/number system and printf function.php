956878a4c628385c1862dc9dcdf8db98bf87984c
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2023-02-12 12:19:42 +0600

    1.0

---
+++ Basic PHP/number system and printf function.php
@@ -0,0 +1,29 @@
+<?php
+//NUMBER SYSTEM
+$n=12;
+$octal=012;
+
+printf("The number is %d and %d <br>",$n,$octal);
+printf("The binary equvalent is %d=%b <br>",1212,1212);
+printf("The octal equvalent is %d=%o <br>",1212,1212);
+printf("The Hexadecimal equvalent is %d=%X <br>",1212,1212);
+printf("The octal equvalent is %d=%o <br>",1212,1212);
+
+//printf function important use
+$fname='Halim';
+$lname='Mallick';
+
+printf('My firstname is %2$s and last name %1$s <br>',$fname,$lname);
+printf('The binary equivalent is %1$d=%1$b <br>',12);
+
+$n=45.126;
+
+printf("%.2lf <br>",$n);
+
+$m=123;
+$b=27;
+
+printf("%08.3f <br>",$m);
+printf("%07.2f <br>",$n);
+
+?>
