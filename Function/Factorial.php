86d4701a0c6f3f716dd1449807a1f724756caad4
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2023-02-14 00:48:46 +0600

    2.0

--- Function/Factorial.php
+++ Function/Factorial.php
@@ -12,10 +12,29 @@ function factorial(/*int*/$s){ //use type hinting for detect integer or not
     }
     return $result;
 }
-
 $in= -1;
 
 factorial($in);
 echo "factorial is $in!=".factorial($in);
 
+?>
+
+
+//Same output using if else
+<?php
+$res=1;
+$in=-1;
+if(gettype($in)!="integer"){
+    echo "not possible";
+}
+elseif($in<0){
+    echo "Negative value not possible";
+}
+else{
+    
+    for($i=1; $i<=$in; $i++){
+    $res*=$i;
+    }
+    echo "$in!=$res";
+}
 ?>
